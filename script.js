5c2b1d326a9f23a9f708d9dbc08a6bd5633a9456
brandonrodriguez6 <37849270+brandonrodriguez6@users.noreply.github.com>
2019-02-22 15:41:52 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript is capitalized using "camel case": https://en.wikipedia.org/wiki/Camel_case
+// Check out the power of the classList property: https://www.w3schools.com/jsref/prop_element_classlist.asp
+
+// "el" stands for the "element" that was just clicked
+
+function showImg(el){
+	document.getElementById("bab").src = "images/baby.png";
+}
